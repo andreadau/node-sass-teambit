bdfde909ddd683799fbf653bb5fe3a7337218e63
Andrea <dauriaandrea99@gmail.com>
2021-01-26 22:58:51 +0100

    Jumbo & HowItWorks Section

--- src/app.js
+++ src/app.js
@@ -24,6 +24,30 @@ let vm = new Vue({
                 icon: "fas",
                 link: "#"
             },
+        ],
+        topCard: [
+            {
+                img: "dist/img/hardcore.png",
+                text: "They are hardcore: formal and disconnected"
+            },            {
+                img: "dist/img/nofeedback.png",
+                text: "They don't encourage regular feedback"
+            },            {
+                img: "dist/img/notsafe.png",
+                text: "They don't make it safe to speak up"
+            },
+        ],
+        bottomCard: [
+            {
+                img: "dist/img/lightweight.png",
+                text: "It's lightweight fun and interconnected"
+            },            {
+                img: "dist/img/feedbacksuggestions.png",
+                text: "It encourages regular feedback with contextual requests"
+            },            {
+                img: "dist/img/safe.png",
+                text: "It creates a safe environment for dialog"
+            },
         ]
     },
     methods: {
